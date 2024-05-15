653f000ee2a480afe962813bdc6ff8bf9b521097
Ephraim Adda <adda.ephraim@gmail.com>
2024-05-15 12:49:17 +0200

    Add commande givecoins.js

---
+++ src/commands/givecoins.js
@@ -0,0 +1,47 @@
+const { ButtonStyle, SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, EmbedBuilder} = require('discord.js');
+const Member = require('../entity/member');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('givecoins')
+        .setDescription('Donner des pièces à un autre membre')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('Membre à qui donner des pièces')
+                .setRequired(true)
+        )
+        .addIntegerOption(option =>
+            option.setName('amount')
+                .setDescription('Montant à donner')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const memberToGive = interaction.options.getMember('user');
+
+        const memberInteractionDB = await Member.getMemberDB(interaction.user.id, interaction.guild.id);
+        const memberToGiveDB = await Member.getMemberDB(memberToGive.id, interaction.guild.id);
+
+        if (!memberInteractionDB) {
+            return interaction.reply({ content: 'Vous n\'avez pas de profil', ephemeral: true });
+        }
+        if (!memberToGiveDB) {
+            return interaction.reply({ content: 'Ce membre n\'a pas de profil', ephemeral: true });
+        }
+        const amount = interaction.options.getInteger('amount');
+
+        if (amount <= 0) {
+            return interaction.reply({ content: 'Le montant doit être supérieur à 0', ephemeral: true });
+        }
+        if (memberInteractionDB.coins < amount) {
+            return interaction.reply({ content: 'Vous n\'avez pas assez de pièces', ephemeral: true });
+        }
+
+        memberInteractionDB.coins -= amount;
+        memberToGiveDB.coins += amount;
+
+        await memberInteractionDB.save();
+        await memberToGiveDB.save();
+
+        return interaction.reply({ content: `Vous avez donné ${amount} pièces à ${memberToGive.user.username}` });
+    }
+}
