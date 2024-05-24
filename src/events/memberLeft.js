00e89064b10ea8a243bd0e043ba2823b008bf370
Ephraim Adda <adda.ephraim@gmail.com>
2024-05-24 20:49:19 +0200

    memberLeft.js Commenté

--- src/events/memberLeft.js
+++ src/events/memberLeft.js
@@ -5,12 +5,12 @@ module.exports = {
     name: 'guildMemberRemove',
     once: false,
     async execute(client, member) {
-        let guildDB = await Guild.getGuildById(member.guild.id);
-        if (!member.user.bot) {
-            let memberDB = await Member.getMemberDB(member.id, guildDB);
-            if (memberDB){
-                await Member.deleteMemberDB(member, guildDB);
-            }
-        }
+        // let guildDB = await Guild.getGuildById(member.guild.id);
+        // if (!member.user.bot) {
+        //     let memberDB = await Member.getMemberDB(member.id, guildDB);
+        //     if (memberDB){
+        //         await Member.deleteMemberDB(member, guildDB);
+        //     }
+        // }
     },
 };
